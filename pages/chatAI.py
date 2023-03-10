32aff084900b666e4061cb26187056db032d71e7
liuyf7 <liuyf7@shanghaitech.edu.cn>
2023-03-10 16:24:36 +0800

    user select temperature

--- pages/chatAI.py
+++ pages/chatAI.py
@@ -4,9 +4,17 @@ import streamlit as st
 
 with st.sidebar:
     st.success("""chatAI使用gpt-3.5-turbo-0301模型，是chatGPT的略微简化版本。""")
+    st.warning("""Tips 小技巧\n
+    1. 把话说清楚点
+    2. Specify the answer format
+    3. 把模型当做一个中学生，
+    让模型逐步思考或讨论利弊。
+    """)
 if 'history' not in st.session_state:
     st.session_state['history'] = []
 usrInput=st.text_area("用户输入")
+TemperatureOptions={"非常严谨":0.0,"严谨":0.2,"平衡":0.4,"富有创造力":0.7}
+modelTemperature=st.select_slider("模型创造力", options=["非常严谨","严谨","富有创造力","整活"],value="严谨")
 if st.button("提交"):
     # region 构造对话
     messageHistory=[{"role": "system", "content": "You are a helpful AI assistant."}]
@@ -18,7 +26,8 @@ if st.button("提交"):
     messageHistory.append({"role": "user", "content": usrInput})
     response=openai.ChatCompletion.create(
       model="gpt-3.5-turbo-0301",
-      messages=messageHistory
+      messages=messageHistory,
+        temperature=TemperatureOptions[modelTemperature],
     )
     # endregion
     st.info("AI回答")
